f3c857fd61c02a163f7451a30868197132b573f8
Félix Dufresne <felduf12@gmail.com>
2023-08-18 00:55:41 -0400

    Camera jitter fix and new collision detection

--- Ragdoll/Assets/Scripts/Player/PositionToParent.cs
+++ Ragdoll/Assets/Scripts/Player/PositionToParent.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 
 public class PositionToParent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void FixedUpdate()
     {
         transform.parent.position = transform.position;
