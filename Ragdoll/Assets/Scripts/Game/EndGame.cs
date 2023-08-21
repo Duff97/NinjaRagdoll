acc7ef13be386f81769f182a299a794635ad038c
duff97 <felix.dufresne.1@ens.etsmtl.ca>
2023-08-21 17:34:05 -0400

    endgame screen

--- Ragdoll/Assets/Scripts/Game/EndGame.cs
+++ Ragdoll/Assets/Scripts/Game/EndGame.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGame : NetworkBehaviour
 {
     public bool gameEnded;
     [SerializeField][SyncVar] float timeUntilGameCloses;
     [SerializeField] private TMP_Text timeUntilGameClosesText;
+    [SerializeField] private Button quitToLobbyBtn;
 
     private NetworkManagerNinjaRagdoll room;
     private NetworkManagerNinjaRagdoll Room
@@ -17,6 +19,14 @@ public class EndGame : NetworkBehaviour
         {
             if (room != null) { return room; }
             return room = NetworkManager.singleton as NetworkManagerNinjaRagdoll;
+        }
+    }
+
+    private void Start()
+    {
+        if (!isServer)
+        {
+            quitToLobbyBtn.interactable = false;
         }
     }
 
