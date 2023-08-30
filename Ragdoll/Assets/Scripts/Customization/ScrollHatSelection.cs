b6eb328f7d2e5d9b56bbe6e38c8b9f8d5ca5b34a
Félix Dufresne <felduf12@gmail.com>
2023-08-29 21:20:35 -0400

    Hat prefabs WIP

--- Ragdoll/Assets/Scripts/Customization/ScrollHatSelection.cs
+++ Ragdoll/Assets/Scripts/Customization/ScrollHatSelection.cs
@@ -1,11 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollHatSelection : MonoBehaviour
 {
-    public void TestButton()
+    [SerializeField] private SelectableHat[] selectableHatPrefabs;
+    [SerializeField] private GameObject hatsContainer;
+
+
+    private void Start()
     {
-        Debug.Log("Button clicked");
+        int i = 0;
+        foreach (var hat in selectableHatPrefabs)
+        {
+            SelectableHat selHat = Instantiate(hat);
+            selHat.transform.SetParent(hatsContainer.transform, false);
+            selHat.hatIndex = i;
+            selHat.gameObject.SetActive(true);
+            i++;
+        }
+
+        SelectableHat.OnHatSelected += HandleSelectedHatChanged;
+    }
+
+    private void HandleSelectedHatChanged(int hatIndex)
+    {
+        // Wear hat
     }
 }
--- Ragdoll/Assets/Scripts/Customization/SelectableHat.cs
+++ Ragdoll/Assets/Scripts/Customization/SelectableHat.cs
@@ -1,10 +1,41 @@
+using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectableHat : MonoBehaviour
 {
-    [SerializeField] GameObject HatPreviewObj;
+    public static event Action<int> OnHatSelected;
+    [HideInInspector] public int hatIndex;
+    [SerializeField] private Color selectedColor;
+    private Image background;
+    private Color defaultColor;
 
-    
+    private void Start()
+    {
+        background = GetComponent<Image>();
+        defaultColor = background.color;
+        OnHatSelected += HandleSelectedHatChanged;
+    }
+
+    public void SelectHat()
+    {
+        background.color = selectedColor;
+        OnHatSelected?.Invoke(hatIndex);
+    }
+
+    public void UnselectHat()
+    {
+        background.color = defaultColor;
+    }
+
+    private void HandleSelectedHatChanged(int hatIndex)
+    {
+        if (hatIndex != this.hatIndex)
+        {
+            background.color = defaultColor;
+        }
+    }
 }
