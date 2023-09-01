c1c8602bfd45281c4d848e6191b1c0a14dac2e5c
Félix Dufresne <felduf12@gmail.com>
2023-08-31 22:33:13 -0400

    Persistent settings

--- Ragdoll/Assets/Scripts/Settings/SettingsMenu.cs
+++ Ragdoll/Assets/Scripts/Settings/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -9,8 +10,17 @@ public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
-
+    public Slider CameraSpeedXSlider;
+    public Slider CameraSpeedYSlider;
+    public Slider VolumeSlider;
+    public Toggle FullScreenToggle;
     private Resolution[] resolutions;
+
+    private const string PPCamSpeedX = "CameraSpeedX";
+    private const string PPCamSpeedY = "CameraSpeedY";
+    private const string PPFullScreen = "FullScreen";
+    private const string PPResolution = "Resolution";
+    private const string PPVolume = "Volume";
 
     private void Start()
     {
@@ -36,6 +46,8 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
 
+        LoadFromPrefs();
+
     }
 
     public void SetVolume (float volume)
@@ -46,5 +58,46 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullScreen(bool isFullSreen)
     {
         Screen.fullScreen = isFullSreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+
+    public void Confirm()
+    {
+        PlayerPrefs.SetFloat(PPCamSpeedX, CameraSpeedXSlider.value);
+        PlayerPrefs.SetFloat(PPCamSpeedY, CameraSpeedYSlider.value);
+        PlayerPrefs.SetFloat(PPVolume, VolumeSlider.value);
+        PlayerPrefs.SetInt(PPFullScreen, FullScreenToggle.isOn ? 1 : 0);
+        PlayerPrefs.SetInt(PPResolution, resolutionDropdown.value);
+    }
+
+    public void LoadFromPrefs()
+    {
+        if (PlayerPrefs.HasKey(PPCamSpeedX))
+            CameraSpeedXSlider.value = PlayerPrefs.GetFloat(PPCamSpeedX);
+
+        if (PlayerPrefs.HasKey(PPCamSpeedY))
+            CameraSpeedYSlider.value = PlayerPrefs.GetFloat(PPCamSpeedY);
+
+        if (PlayerPrefs.HasKey(PPVolume))
+        {
+            VolumeSlider.value = PlayerPrefs.GetFloat(PPVolume);
+            SetVolume(VolumeSlider.value);
+        }
+
+        if (PlayerPrefs.HasKey(PPResolution))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt(PPResolution);
+            SetResolution(resolutionDropdown.value);
+        }
+
+        if (!PlayerPrefs.HasKey(PPFullScreen))
+        {
+            FullScreenToggle.SetIsOnWithoutNotify(PlayerPrefs.GetInt(PPFullScreen) == 1 ? true : false);
+            SetFullScreen(FullScreenToggle.isOn);
+        }
     }
 }
