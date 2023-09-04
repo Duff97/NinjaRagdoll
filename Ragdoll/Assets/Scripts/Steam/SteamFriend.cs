140226797718f4fcebb4be11c2cb9e7e292e0519
Félix Dufresne <felduf12@gmail.com>
2023-09-04 18:07:15 -0400

    steam friends avatar

--- Ragdoll/Assets/Scripts/Steam/SteamFriend.cs
+++ Ragdoll/Assets/Scripts/Steam/SteamFriend.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SteamFriend : MonoBehaviour
 {
     [SerializeField] private TMP_Text nameText;
     [SerializeField] private TMP_Text statusText;
     [SerializeField] private string steamAppId;
+    [SerializeField] private RawImage profileImage;
     public void DisplaySteamInfo(CSteamID steamID)
     {
         nameText.text = SteamFriends.GetFriendPersonaName(steamID);
@@ -26,5 +28,35 @@ public class SteamFriend : MonoBehaviour
             }
         }
 
+        int imageId = SteamFriends.GetLargeFriendAvatar(steamID);
+
+        if (imageId != -1)
+        {
+            profileImage.texture = GetSteamImageAsTexture(imageId);
+        }
+
+    }
+
+    private Texture2D GetSteamImageAsTexture(int iImage)
+    {
+        Texture2D texture = null;
+
+        bool isValid = SteamUtils.GetImageSize(iImage, out uint width, out uint height);
+
+        if (isValid)
+        {
+            byte[] image = new byte[width * height * 4];
+
+            isValid = SteamUtils.GetImageRGBA(iImage, image, (int)(width * height * 4));
+
+            if (isValid)
+            {
+                texture = new Texture2D((int)width, (int)height, TextureFormat.RGBA32, false, true);
+                texture.LoadRawTextureData(image);
+                texture.Apply();
+            }
+        }
+
+        return texture;
     }
 }
